1232d3d80d77ad867cdcee44f1387020742ab0b5
username123212312 <yousefrazouk888@gmail.com>
2025-11-12 22:45:22 +0300

    called userPlay()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         String[][] board = State.initialBoard();
         new State(board);
-        new Game(board.length, board[0].length);
+        Game g = new Game(board.length, board[0].length);
+        g.userPlay(new State(board), Rabbit.findAllRabbits(board));
     }
 }
