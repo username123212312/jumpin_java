bd8c4e143af235f794e621997477f5ef8da09dc8
yasmin Ebrahim <yasmineebrahim458@gmail.com>
2025-11-16 21:37:45 +0200

    add Hole class

--- src/main/java/org/example/Hole.java
+++ src/main/java/org/example/Hole.java
@@ -1,8 +1,27 @@
-package org.example;
 
 
-class Hole extends Entity{
+public class Hole extends Entity {
+    private Rabbit occupant; 
+
     public Hole(int x, int y) {
         super(x, y);
+        this.occupant = null;    
+    }
+
+    public void setOccupant(Rabbit rabbit) {
+        this.occupant = rabbit;
+    }
+
+    public void removeOccupant() {
+        this.occupant = null;
+    }
+
+    public boolean isOccupied() {
+        return occupant != null;
+    }
+
+    public Rabbit getOccupant() {
+        return occupant;
     }
 }
+
